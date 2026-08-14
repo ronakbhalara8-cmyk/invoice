73a1926aa8d06d66ba84dd047191c08b5d0d5e5a
ronakbhalara8-cmyk <ronakbhalara8@gmail.com>
2026-08-14 18:14:31 +0530

    changes

--- src/app/api/items/[id]/route.js
+++ src/app/api/items/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import db from '../../../lib/db';
+import db from '../../../../lib/db';
 import jwt from 'jsonwebtoken';
 import fs from 'fs';
 import path from 'path';
